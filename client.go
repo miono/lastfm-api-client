b9cd063d57c1ba053a101975e13a459487017743
Markus Nilsson <markus.nilsson@pricerunner.com>
2018-08-22 08:45:24 +0200

    GetRecentTracks now works and returns a slice of tracks

--- client.go
+++ client.go
@@ -26,15 +26,55 @@ func NewClient(apikey string) *Client {
 	}
 }
 
-// Track defines a track
-type Track struct {
-	Artist string // `json:"artist"`
-	Title  string // `json:"title"`
+// GetRecentTracks contains the full response from the getrecenttracks-method
+type GetRecentTracks struct {
+	RecentTracks `json:"recenttracks"`
+}
+
+// RecentTracks is the name of the list of tracks returned
+type RecentTracks struct {
+	RecentTracks []RecentTrack `json:"track"`
+}
+
+// RecentTrack is a track-struct
+type RecentTrack struct {
+	Artist     `json:"artist"`
+	Name       string `json:"name"`
+	Streamable string `json:"streamable"`
+	Mbid       string `json:"mbid"`
+	Album      `json:"album"`
+	URL        string  `json:"url"`
+	Image      []Image `json:"image"`
+	Date       `json:"date"`
+}
+
+// Artist is the mbid and name of an artist
+type Artist struct {
+	Text string `json:"#text"`
+	Mbid string `json:"mbid"`
+}
+
+// Album is the mbid and name of an album
+type Album struct {
+	Text string `json:"#text"`
+	Mbid string `json:"mbid"`
+}
+
+// Image is the images for the album together with their sizes
+type Image struct {
+	Text string `json:"#text"`
+	Sixe string `json:"size"`
+}
+
+// Date is the date when the track was played
+type Date struct {
+	Uts  string `json:"uts"`
+	Text string `json:"text"`
 }
 
 // GetRecentTracks gets the recent tracks for a user
-func (c *Client) GetRecentTracks(user string) []Track {
-	url := fmt.Sprintf("method=user.getrecenttracks&user=%v&api_key=%v&format=json", user, c.Apikey)
+func (c *Client) GetRecentTracks(user string) []RecentTrack {
+	url := fmt.Sprintf("method=user.getrecenttracks&user=%v&api_key=%v&format=json&limit=3", user, c.Apikey)
 	reqURL := c.BaseURL + url
 	response, err := c.httpClient.Get(reqURL)
 	if err != nil {
@@ -44,12 +84,16 @@ func (c *Client) GetRecentTracks(user string) []Track {
 	if err != nil {
 		panic(err)
 	}
-	var out Track
-	json.Unmarshal(responseSlice, &out)
-	var tracks []Track
-	track := Track{"kent", "747"}
-	tracks = append(tracks, track)
-	track = Track{"kent", "Våga vara rädd"}
-	tracks = append(tracks, track)
+	var ret GetRecentTracks
+	var tracks []RecentTrack
+	err = json.Unmarshal(responseSlice, &ret)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(ret.RecentTracks.RecentTracks[0])
+	for _, track := range ret.RecentTracks.RecentTracks {
+		tracks = append(tracks, track)
+	}
+
 	return tracks
 }
